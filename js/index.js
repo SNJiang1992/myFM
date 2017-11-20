d5f197f96bd3f195aeaab2d9cb6756f477d78f0d
john <619805205@qq.com>
2017-11-20 14:54:52 +0800

    歌词显示 细节优化

--- js/index.js
+++ js/index.js
@@ -79,7 +79,7 @@ let Footer = {
     },
     render() {
         let _this = this
-        $.getJSON('http://api.jirengu.com/fm/getChannels.php')
+        $.getJSON('//jirenguapi.applinzi.com/fm/getChannels.php')
             .done((ret) => {
                 _this.renderFooter(ret.channels)
             }).fail(function () {
@@ -126,17 +126,17 @@ let Fm = {
             let $btn = $(this)
             if ($btn.hasClass('icon-play')) {
                 $btn.removeClass('icon-play').addClass('icon-pause')
+                $btn.find('img').attr('src', 'imgs/Pause.png')
                 _this.audio.play()
             } else {
                 $btn.removeClass('icon-pause').addClass('icon-play')
+                $btn.find('img').attr('src', 'imgs/Play.png')
                 _this.audio.pause()
             }
         })
 
         this.$container.find('.btn-next').on('click', function () {
-            _this.loadMusic(function () {
-                _this.setMusic()
-            })
+            _this.loadMusic()
         })
 
         this.audio.addEventListener('play', function () {
@@ -146,36 +146,57 @@ let Fm = {
             }, 1000)
         })
 
-        this.audio.addEventListener('pause',function () {
+        this.audio.addEventListener('pause', function () {
             clearInterval(_this.statusClock)
         })
     },
     loadMusic(callback) {
         let _this = this
-        console.log('loadmusic...')
         $.getJSON('//jirenguapi.applinzi.com/fm/getSong.php', {channel: this.channelId})
             .done(function (ret) {
-                console.log(ret['song'][0])
                 _this.song = ret['song'][0]
-                callback()
+                _this.setMusic()
+                _this.loadLyric()
             })
     },
+
+    loadLyric() {
+        let _this = this
+        $.getJSON('//jirenguapi.applinzi.com/fm/getLyric.php', {sid: this.song.sid}).done(function (ret) {
+            let lyric = ret.lyric
+            let lyricObj = {}
+            lyric.split('\n').forEach(function (line) {
+                let times = line.match(/\d{2}:\d{2}/g)//匹配前面的时间部分
+                let str = line.replace(/\[.+?\]/g, '')//取出单独的文本
+                if (Array.isArray(times)) {
+                    times.forEach(function (time) {
+                        lyricObj[time] = str
+                    })
+                }
+            })
+            _this.lyricObj = lyricObj
+        })
+    },
+
     setMusic() {
-        console.log('set music...')
-        console.log(this.song)
         this.audio.src = this.song.url
         $('.bg').css('background-image', `url('${this.song.picture}')`)
         this.$container.find('.aside figure').css('background-image', `url('${this.song.picture}')`)
         this.$container.find('.detail h1').text(this.song.title)
         this.$container.find('.detail .author').text(this.song.artist)
         this.$container.find('.tag').text(this.channelName)
+        this.$container.find('.btn-play').removeClass('icon-play').addClass('icon-pause')
     },
     updataStatus() {
-        let min = Math.floor(this.audio.currentTime/60)
-        let second = Math.floor(this.audio.currentTime%60)+''
-        second = second.length ===2?second:`0${second}`
-        this.$container.find('.current-time').text(min+':'+second)
-        this.$container.find('.bar-progress').css('width',this.audio.currentTime/this.audio.duration*100+'%')
+        let min = Math.floor(this.audio.currentTime / 60)
+        let second = Math.floor(this.audio.currentTime % 60) + ''
+        second = second.length === 2 ? second : `0${second}`
+        this.$container.find('.current-time').text(min + ':' + second)
+        this.$container.find('.bar-progress').css('width', this.audio.currentTime / this.audio.duration * 100 + '%')
+        let line = this.lyricObj['0' + min + ':' + second]
+        if (line) {
+            this.$container.find('.lyric p').text(line)
+        }
     }
 }
 
